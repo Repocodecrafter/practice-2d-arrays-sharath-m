327ced4085214c9683a2d5da954fc50a75b4d0b5
Sharath Meghana <marellameghana99@gmail.com>
2025-10-16 23:35:09 -0500

    2D array programs showing student results, seating grid, and contacts info

---
+++ practice-2d-arrays.js
@@ -0,0 +1,11 @@
+//Chalenge 1
+let allScores = [[74, 85, 96], [95, 89, 100], [75, 6, 92]];
+console.log("All student scores: ", allScores);
+
+//Challenge2
+let theaterSeating = [["A1", "A2", "A3", "A4"], ["B1", "B2", "B3", "B4"], ["C1", "C2", "C3", "C4"]];
+console.log("Theater Seating Grid:", theaterSeating);
+
+//Challenge3
+let contactsList = [["Ashley","ashley12@gmail.com", "+1 202-534-0892"], ["David", "rsdavid902@gmail.com", "+1 945-092-0916"]];
+console.log("Contacts List: ", contactsList);
